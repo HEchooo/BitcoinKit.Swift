0b968f7e828b79f064bed4273c2cdb37a573bab0
Robbins <huangxin@valleysound.xyz>
2024-10-10 10:52:09 +0800

    flxed：依赖 BitcoinCore 指定分支

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["BitcoinKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/HEchooo/BitcoinCore.Swift.git", .upToNextMinor(from: "2.1.3")),
+        .package(url: "https://github.com/HEchooo/BitcoinCore.Swift.git", branch: "flxed/grdb"),
         .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMinor(from: "1.2.1")),
         .package(url: "https://github.com/HEchooo/Hodler.Swift.git", .upToNextMajor(from: "2.0.1")),
         .package(url: "https://github.com/HEchooo/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.5")),
